8dcd407b96faadda424edfe4c7baed645afbb285
Paolo Bernacca <bernaccapaolo@gmail.com>
2023-05-10 20:00:10 +0200

    Stub implementation atari dqn

--- playground/dqn.py
+++ playground/dqn.py
@@ -6,33 +6,67 @@ from torchinfo import summary
 from tqdm import tqdm
 import torchvision
 from torchvision import transforms
+from itertools import count
 import matplotlib.pyplot as plt
-from collections import deque
+from collections import deque, namedtuple
+import random
 
 import argparse
+
+# Utils
+import logging
+def disable_logging(func):
+    def wrapper(*args, **kwargs):
+        # Disable logging
+        logging.disable(logging.CRITICAL)
+
+        # Call the original function
+        result = func(*args, **kwargs)
+
+        # Re-enable logging
+        logging.disable(logging.NOTSET)
+
+        return result
+    return wrapper
 
 # IMPLEMENTATION OF: Playing Atari with Deep Reinforcement Learning
 # https://arxiv.org/pdf/1312.5602.pdf and https://arxiv.org/pdf/1711.07478.pdf and https://storage.googleapis.com/deepmind-media/dqn/DQNNaturePaper.pdf
 
+
 # Parse commandline for game name and other infos.
 def parse_args():
-    parser = argparse.ArgumentParser(description='Parse command line arguments.')
-    parser.add_argument('--game', type=str, default='Cartpole', help='Name of the game to play')
-    parser.add_argument('--training', action='store_true', help='Enable training mode')
+    parser = argparse.ArgumentParser(description="Deep Q-Network (DQN) algorithm with PyTorch and Atari Games (Gym Env).")
+    parser.add_argument("--game", type=str, default="Pong", help="Name of the game to play")
+    parser.add_argument("--training", action="store_true", default=True, help="Enable training mode")
+    parser.add_argument("--tau", type=float, default=1, help="Value of tau for soft target network update (default: 1)") # 1 for hard update, <1 for soft updates.
+    parser.add_argument("--align-models-every", type=int, default=1, help="Number of steps between aligning the policy and target networks (default: 10000)")
     args = parser.parse_args()
-    return args.game.lower(), args.training
+    return args.game.lower(), args.training, args.tau, args.align_models_every
 
-name_to_env_names = {
-    "pong": "Pong-v4",
-    "cartpole": "CartPole-v1"
-}
+name_to_env_names = {"pong": "Pong-v4"}
 
-requested_game, training = parse_args()
+Transition = namedtuple("Transition", ("State", "Action", "Reward", "Next_State"))
+
+requested_game, training, TAU, align_models_every_nstep = parse_args()
 env_name = name_to_env_names[requested_game]
 
-is_atari_game = requested_game != 'cartpole'
-
 print(f"Selected Enviroment: {env_name}")
+
+initial_samples_size = 50000  # initial samples to collect with a random policy
+linear_decay_episodes = 1000000
+eps_greedy_start = 1
+eps_greedy_end = 0.1
+replay_buffer_size = 1000000
+total_train_frames = 10000000
+skip_frames = 4 if not env_name.startswith("SpaceInvaders") else 3
+gamma = 0.99
+batch_size = 32
+obs_stack_dim = 4
+learning_rate = 0.00025
+gradient_momentum = 0.95
+squared_gradient_momentum = 0.95
+min_squared_gradient = 0.1
+
 
 # Deep Q-Network approximates a state-value function in a Q-Learning framework with a neural network.
 # It's similare to Q-Learning with some main differences:
@@ -40,117 +74,273 @@ print(f"Selected Enviroment: {env_name}")
 # 1) Each step update is, potentially, used in many weights updates, which allow for greater data efficency.
 # 2) Learning directly from consecutive samples is inefficient, due to the strong correlations between the samples; randoming the samples breaks these correlations and therefore
 #   reduces the variance of the updates
-# 3) When learning on-policy the current parameter determine the next data sample that the parameters are trained on. By using experience replay the behavior distribution is averaged 
+# 3) When learning on-policy the current parameter determine the next data sample that the parameters are trained on. By using experience replay the behavior distribution is averaged
 #   over many of its previous states, smoothing out learning and avoid oscillations or diverrgence in the parameters.
 # NOTE: when learning by experience replay. it is necessary to learn off-policy. (that's why of Q-Learning)
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
 def create_image_preprocessor():
     """
-        IMAGE PREPROCESSING:
-        Atari frame are 210x160 pixels with a 128 color palette, that a challenging amount of data to handle; we do few steps to reduce the dimensionality:
-        1) We convert the frame into a gray-scale
-        2) Down-sampling it to a 110x84 image. 
-        3) Cropping an 84x84 region 
+    IMAGE PREPROCESSING:
+    Atari frame are 210x160 pixels with a 128 color palette, that a challenging amount of data to handle; we do few steps to reduce the dimensionality:
+    1) We convert the frame into a gray-scale
+    2) Down-sampling it to a 110x84 image.
+    3) Cropping an 84x84 region
     """
-    return transforms.Compose(transforms=[
-        transforms.ToTensor(),
-        transforms.Grayscale(),
-        transforms.Resize((110, 84), antialias=True),
-        transforms.CenterCrop((84,84))
-    ])
-    
+    return transforms.Compose(
+        transforms=[
+            transforms.ToPILImage(),
+            transforms.Resize((110, 84), antialias=True),
+            transforms.CenterCrop((84, 84)),
+            transforms.Grayscale(),
+            transforms.ToTensor()
+        ]
+    )
+
+
 image_transform = create_image_preprocessor()
 
 
 # Replay Buffers are a central part of off-policy RL algorithms.
 
 # A replay buffer is a data structure that stores experiences of the agent as it interacts with the environment.
-# Each experience is typically represented as a tuple containing the current state, the action taken, the resulting reward, and the next state. 
+# Each experience is typically represented as a tuple containing the current state, the action taken, the resulting reward, and the next state.
 # The replay buffer can be thought of as a dataset of experiences that the agent can use to learn from.
-
 class ReplayBuffer:
-    def __init__(self, max_size):
-        self.buffer = deque(maxlen=max_size)
+    def __init__(self, max_size, device="cpu"):
+        self.buffer = deque([], maxlen=max_size)
+        self.device = device
 
     # Add a new experience to the buffer.
-    def add(self, experience):
-        self.buffer.append(experience)
+    def add(self, state, action, rew, next_state):
+        self.buffer.append(
+            (
+                state.to(self.device),
+                action.to(self.device),
+                rew.to(self.device),
+                next_state.to(self.device) if next_state is not None else None,
+            )
+        )
 
     # Samples a batch of experiences from the buffer of size batch_size and returns the experiences.
     def sample(self, batch_size):
-        batch = np.random.choice(self.buffer, size=batch_size, replace=False)
+        batch = random.sample(self.buffer, batch_size)
         return batch
-    
+
     # This method returns the number of experiences currently stored in the buffer.
     def __len__(self):
         return len(self.buffer)
-        
 
 
-# BUNCH OF TRAINING HYPERPARAMETERS
-initial_samples_size = 50000 # initial samples to collect with a random policy
-linear_decay_episodes = 1000000
-eps_greedy_start = 1
-eps_greedy_end = 0.1
-replay_buffer_size = 1000000
-total_train_frames = 10000000
-skip_frames = 4 if not env_name.startswith('SpaceInvaders') else 3
-gamma = 0.99
-batch_size = 32
-obs_stack_dim = 4 if is_atari_game else 1
+# Simple utils rollout buffer. This is used to stack N-frames together.
+class RolloutBuffer():
+    def __init__(self, max_size, device="cpu"):
+        self.buffer = deque([], maxlen=max_size)
+        self.device=device
+
+    # init the buffer to various copies of same experience.
+    def fill(self, experience):
+        self.buffer.clear()
+        for i in range(self.buffer.maxlen):
+            self.add(experience)
+
+    # Add a new experience to the buffer.
+    def add(self, experience):
+        self.buffer.append(experience.to(self.device))
+
+    # get buffers. stacked together
+    def get(self):
+        return torch.cat(tuple(trajectory_container.buffer))
 
 # Create atari game environment
-if is_atari_game:
-    env = gym.make(env_name, render_mode = None if training else 'human', frameskip=skip_frames)
-else:
-    env = gym.make(env_name, render_mode = None if training else 'human')
+env = gym.make(
+    env_name, render_mode=None if training else "human", frameskip=skip_frames
+)
 
 
 # Replica of the model presented in the paper. Is a standard CNN.
 class DQNModel(torch.nn.Module):
-    def __init__(self, obs_size : int, action_size : int) -> None:
+    def __init__(self, obs_size: int, action_size: int) -> None:
         super().__init__()
 
         self.conv1_block = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=obs_size, out_channels=16, kernel_size=(8, 8), stride=4), # (N, 4, 84, 84) -> (N, 16, 20, 20)
-            torch.nn.ReLU()
-        ) 
+            torch.nn.Conv2d(
+                in_channels=obs_size, out_channels=32, kernel_size=(8, 8), stride=4
+            ),  # (N, 4, 84, 84) -> (N, 32, 20, 20)
+            torch.nn.ReLU(),
+        )
 
         self.conv2_block = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(4, 4), stride=2), # (N, 4, 84, 84) -> (N, 16, 20, 20)
-            torch.nn.ReLU()
-        ) 
+            torch.nn.Conv2d(
+                in_channels=32, out_channels=64, kernel_size=(4, 4), stride=2
+            ),  # (N, 32, 20, 20) -> (N, 64, 9, 9)
+            torch.nn.ReLU(),
+        )
+
+        self.conv3_block = torch.nn.Sequential(
+            torch.nn.Conv2d(
+                in_channels=64, out_channels=64, kernel_size=(3, 3), stride=1
+            ),  # (N, 64, 9, 9) -> (N, 64, 7, 7)
+            torch.nn.ReLU(),
+        )
 
         self.classifier = torch.nn.Sequential(
             torch.nn.Flatten(),
-            torch.nn.Linear(in_features=2592, out_features=256),
+            torch.nn.Linear(in_features=3136, out_features=512),  # 3136 = 64*7*7
             torch.nn.ReLU(),
-            torch.nn.Linear(in_features=256, out_features=action_size)
+            torch.nn.Linear(in_features=512, out_features=action_size),
         )
 
     def forward(self, x):
         x = self.conv1_block(x)
         x = self.conv2_block(x)
+        x = self.conv3_block(x)
 
         return self.classifier(x)
-    
+
+
+# select action with eps_greedy param.
+@torch.inference_mode()
+def select_action(model: torch.nn.Module, obs, eps_greedy: float):
+    if np.random.random() > eps_greedy:
+        return model(obs).argmax()
+    else:
+        return torch.tensor(env.action_space.sample(), device=device)
+
 
 # eps scheduling.
 def compute_eps(current_step, min, max, num_steps):
     alpha = current_step / num_steps
     alpha = np.clip(alpha, 0.0, 1.0)
-
     return min * (1 - alpha) + max * alpha
 
 
-# init replay buffer
-replay_buffer = ReplayBuffer(max_size=replay_buffer_size)
-
 # init action-value Q
-obs_dim = 1 # after image processing we will have 1 color channel.
+obs_dim = (4, 84, 84)  # after image processing we will have 1 color channel.
 action_dim = env.action_space.n
 
-q_model = DQNModel(1, action_size=action_dim).to(device)
-summary(q_model, input_size=(1,1,84,84))
+# create models.
+policy_model = DQNModel(obs_dim[0], action_size=action_dim).to(device)
+target_model = DQNModel(obs_dim[0], action_size=action_dim).to(device)
+
+target_model.load_state_dict(policy_model.state_dict())
+
+summary(policy_model, input_size=(batch_size, *obs_dim))
+
+def sample_mean(arr, num_samples):
+    sample_size = len(arr) // num_samples
+    mean_arr = np.zeros(num_samples)
+    for i in range(num_samples):
+        start = i * sample_size
+        end = start + sample_size
+        mean_arr[i] = np.mean(arr[start:end])
+    return mean_arr
+
+# optimizer = torch.optim.RMSprop(policy_model.parameters(), lr=learning_rate, momentum=gradient_momentum, alpha=squared_gradient_momentum, eps=0.01)
+optimizer = torch.optim.AdamW(policy_model.parameters(), lr=learning_rate)
+steps_done = 0
+
+# init replay buffer
+replay_buffer = ReplayBuffer(replay_buffer_size, "cpu")
+trajectory_container = RolloutBuffer(obs_dim[0], "cpu")
+
+# fill replay buffer first.
+def initial_fill_replay_buffer(replay_buffer: ReplayBuffer, size: int):
+    progress_bar = tqdm(
+        total=size, 
+        desc="Initialize replay buffer with random experiences.", 
+        position=0
+    )
+    progress_bar.set_description_str("Filling replay_buffer with random experiences")
+    progress_bar.update(len(replay_buffer))
+
+    while len(replay_buffer) < size:
+        state, info = env.reset()
+        state = image_transform(state)
+        trajectory_container.fill(state)
+
+        for _ in count():
+            current_state = trajectory_container.get()
+
+            action = select_action(None, current_state, 1.0)  # always random action
+            obs, rew, terminated, truncated, info = env.step(action.item())
+
+            rew = torch.tensor(rew)
+            done = terminated or truncated
+
+
+            if done:
+                next_state = None
+            else:
+                trajectory_container.add(image_transform(obs))
+                next_state = trajectory_container.get() 
+
+            replay_buffer.add(current_state, action, rew, next_state)
+            progress_bar.update(1)
+
+            if done or len(replay_buffer) >= size:
+                break
+    
+
+def optimize(model: torch.nn.Module, stale_model: torch.nn.Module, replay_buffer: ReplayBuffer):
+    # TODO: implement me
+    pass
+
+
+@disable_logging
+def align_models(dict):
+    #target_model.load_state_dict(policy_model.state_dict())
+    # Soft update of the target network's weights. if TAU is 1 it is the same as doing an Hard-update
+    # θ′ ← τ θ + (1 −τ )θ′
+    target_net_state_dict = target_model.state_dict()
+    for key in dict:
+        target_net_state_dict[key] = dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
+    target_model.load_state_dict(target_net_state_dict)
+
+progress_bar = tqdm(total=total_train_frames, desc="Training progress.", position=0)
+scores = []
+
+steps_done = 0
+while steps_done < total_train_frames:
+        obs, info = env.reset()
+        obs = image_transform(obs)
+        trajectory_container.fill(obs)
+        score = 0
+
+        for _ in count():
+            current_state = trajectory_container.get()
+            
+            eps = compute_eps(steps_done, eps_greedy_start, eps_greedy_end, linear_decay_episodes)
+            action = select_action(policy_model, current_state, eps)
+
+            obs, rew, terminated, truncated, info = env.step(action.item())
+            score += rew
+            rew = torch.tensor(rew)
+
+            done = terminated or truncated
+
+            if done:
+                next_state = None
+            else:
+                trajectory_container.add(image_transform(obs))
+                next_state = trajectory_container.get()
+
+            replay_buffer.add(current_state, action, rew, next_state)
+
+            optimize(policy_model, target_model, replay_buffer)
+
+            # Align the two network weights. Either soft update or hard update.
+            if (steps_done % align_models_every_nstep == 0):
+                align_models(policy_model.state_dict())
+            
+            # Update training informations.
+            if done:
+                scores.append(score)
+                scores_num = len(scores)
+                avg_score = np.mean(scores[np.max([-scores_num, -50]):-1]) if scores_num > 1 else score
+                progress_bar.set_description_str(f"Avg Score: {avg_score:.2f}")
+                progress_bar.update(1)
+                break
+
+
